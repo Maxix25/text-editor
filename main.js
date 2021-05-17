fa1146e1098e9a787271857525e057e438fdaf53
Maximiliano <maximiliano.besoain@gmail.com>
2021-05-17 13:02:01 -0400

    Fix editor

--- main.js
+++ main.js
@@ -8,25 +8,6 @@ const pythonEditor = CodeMirror.fromTextArea(
   }
 );
 
-const pythonEditor = CodeMirror.fromTextArea(
-  document.getElementById("python"),
-  {
-    mode: "python",
-    lineNumbers: true,
-    theme: "base16-dark",
-    autoCloseBrackets: true,
-  }
-);
-const pythonEditorVim = CodeMirror.fromTextArea(
-  document.getElementById("python-vim"),
-  {
-    mode: "python",
-    lineNumbers: true,
-    theme: "base16-dark",
-    autoCloseBrackets: true,
-    keyMap: "vim",
-  }
-);
 
 const htmlEditor = CodeMirror.fromTextArea(document.getElementById("html"), {
   mode: "xml",
