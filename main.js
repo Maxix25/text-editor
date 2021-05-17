a44bfc8975aae239b27b67a43e83b675d182f01d
pruthvi10 <pruthvimohanlal10@gmail.com>
2021-05-17 17:52:15 +0100

    layout changes

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const pythonEditor = CodeMirror.fromTextArea(document.getElementById("python"),{
-	mode: "python",
-	lineNumbers:true,
-	theme:"monokai",
-	autoCloseBrackets:true
-});
+const pythonEditor = CodeMirror.fromTextArea(
+  document.getElementById("python"),
+  {
+    mode: "python",
+    lineNumbers: true,
+    theme: "base16-dark",
+    autoCloseBrackets: true,
+  }
+);
 const htmlEditor = CodeMirror.fromTextArea(document.getElementById("html"), {
-	mode:"xml",
-	lineNumbers:true,
-	theme:"monokai",
-	autoCloseTags:true
-})
+  mode: "xml",
+  lineNumbers: true,
+  theme: "base16-dark",
+  autoCloseTags: true,
+});
