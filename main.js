3324fce404804ba91e4721573eed27b0ab5c9b66
Maximiliano <maximiliano.besoain@gmail.com>
2021-05-17 11:02:14 -0400

    Add all files

---
+++ main.js
@@ -0,0 +1,10 @@
+const pythonEditor = CodeMirror.fromTextArea(document.getElementById("python"),{
+	mode: "python",
+	lineNumbers:true,
+	theme:"dracula"
+});
+const htmlEditor = CodeMirror.fromTextArea(document.getElementById("html"), {
+	mode:"xml",
+	lineNumbers:true,
+	theme:"dracula"
+})
