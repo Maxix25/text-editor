c98b09e3048bd6a3ff161a8cba5b3e57c702e0c4
Maximiliano <maximiliano.besoain@gmail.com>
2021-05-19 19:34:25 -0400

    Add JavaScript highlight

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 const pythonEditor = CodeMirror.fromTextArea(
   document.getElementById("python"),
   {
@@ -14,3 +15,10 @@ const htmlEditor = CodeMirror.fromTextArea(document.getElementById("html"), {
   theme: "base16-dark",
   autoCloseTags: true,
 });
+*/
+const jsEditor = CodeMirror.fromTextArea(document.getElementById("js"), {
+	mode: "javascript",
+	lineNumbers: true,
+	theme: "base16-dark",
+	autoCloseBrackets: true
+})
